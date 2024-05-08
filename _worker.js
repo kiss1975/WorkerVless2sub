f6994ca964e4ea337f845b5f867b465204443854
kiss1975 <128604330+kiss1975@users.noreply.github.com>
2024-05-08 13:53:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -73,7 +73,6 @@ let addressesapi = [
 	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 	'https://raw.githubusercontent.com/ymyuuu/IPDB/main/bestcf.txt',
-	'https://raw.githubusercontent.com/878088/Worker/main/TLS.txt',
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
